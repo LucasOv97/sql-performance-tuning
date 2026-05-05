7a85df5e9d341f144547384f6bbb30e7e5648af4
LucasOv97 <96752051+LucasOv97@users.noreply.github.com>
2026-05-05 10:03:05 -0300

    Update 03_optimized_queries.sql

--- scripts/03_optimized_queries.sql
+++ scripts/03_optimized_queries.sql
@@ -4,6 +4,6 @@ CREATE INDEX idx_sales_date ON Sales(SaleDate);
 -- Query optimizada
 SELECT CustomerId, SUM(Amount)
 FROM Sales
-WHERE SaleDate >= '2024-01-01'
-  AND SaleDate < '2025-01-01'
+WHERE SaleDate >= '2025-01-01'
+  AND SaleDate < '2026-01-01'
 GROUP BY CustomerId;
